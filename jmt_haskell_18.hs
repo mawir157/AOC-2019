610952106ff91838ea8c4f26d5ab89fc8ef56a0a
james thompson <jamesmthompson2@gmail.com>
2020-05-26 14:33:08 +0100

    SLOW!

--- jmt_haskell_18.hs
+++ jmt_haskell_18.hs
@@ -28,13 +28,14 @@ parseLine n s = Map.fromList $ zip ps s
         ps = zip (repeat n) [0..(i-1)]
 
 -- (start, open, keys, doors)
-parseCave :: RawCave -> (Set.Set Pos, Set.Set Pos, [PoI], [PoI])
-parseCave rc = (initial, open', keys, doors)
+parseCave :: RawCave -> (Set.Set Pos, KnD)
+parseCave rc = (open', places)
   where initial = Set.fromList $ Map.keys $ Map.filter (== '@') rc
         open = Set.fromList $ Map.keys $ Map.filter (== '.') rc
-        keys = map (swap) $ Map.toList $ Map.filter (`elem` ['a'..'z']) rc
-        doors = map (swap) $ Map.toList $ Map.filter (`elem` ['A'..'Z']) rc
-        open' = Set.union (Set.fromList $ map snd keys) $ Set.union initial open
+        keys = Map.fromList $ map (swap) $ Map.toList $ Map.filter (`elem` (['a'..'z'] ++ "@")) rc
+        doors = Map.fromList $ map (swap) $ Map.toList $ Map.filter (`elem` ['A'..'Z']) rc
+        places = Map.union keys doors
+        open' = Set.union (Set.fromList $ Map.elems keys) $ Set.union initial open
 --------------------------------------------------------------------------------
 neighbours :: Set.Set Pos -> Pos -> Set.Set Pos
 neighbours c (x,y) = Set.intersection p c
@@ -74,10 +75,10 @@ type AStarState = (Pos, Pos, Set.Set Pos, Map.Map Pos Pos,
 insertTuples :: (Ord k) => [(k,a)] -> Map.Map k a -> Map.Map k a
 insertTuples pairs ms = foldr (\(k,a) m -> Map.insert k a m) ms pairs
 
-distKeys :: Set.Set Pos -> [PoI] -> Char -> Char -> Int
+distKeys :: Set.Set Pos -> KnD -> Char -> Char -> Int
 distKeys stack keys start goal = length $ aStar stack from to
-  where from = snd $ fromJust $ find (\(c,_) -> c == start) keys
-        to   = snd $ fromJust $ find (\(c,_) -> c == goal) keys
+  where from = fromJust $ Map.lookup start keys
+        to   = fromJust $ Map.lookup goal keys
 
 distKeys' :: Set.Set Pos -> Pos -> Pos -> Int
 distKeys' stack start goal = length $ aStar stack start goal
@@ -108,37 +109,69 @@ parseToEdge ss = Just (head next, 1 + length dots)
   where dots = takeWhile (\c -> c == '.') ss
         next = dropWhile (\c -> c == '.') ss
 
-printString :: RawCave -> [Pos] -> String
-printString rc route = printHelper ss
-  where ss = map (\x -> fromJust $ Map.lookup x rc) route
+-- printString :: RawCave -> [Pos] -> String
+-- printString rc route = printHelper ss
+--   where ss = map (\x -> fromJust $ Map.lookup x rc) route
 
-printHelper :: String -> String
-printHelper []  = []
-printHelper s = " --" ++ (show $ (1 + length dots)) ++ "-> "  ++ [(head next)] ++ (printHelper $ drop 1 next) 
+undoString :: Char -> String -> (Char, Int)
+undoString c s
+  | length s /= 0 = (head next, (1 + (length dots)))
+  | otherwise     = (c, 0)
   where dots = takeWhile (\c -> c == '.') s
         next = dropWhile (\c -> c == '.') s
 
-getPaths :: Set.Set Pos -> [PoI] -> Char -> Char -> [(Integer, Integer)]
+undoString2 :: Char -> String -> (Char, (Int, String))
+undoString2 c s
+  | length s /= 0 = (last s, (length s, locks))
+  | otherwise     = (c, (0, ""))
+  where locks = filter (`elem` ['A'..'Z']) $ filter ( /= '.' ) $ init s
+
+-- printHelper :: String -> String
+-- printHelper [] = []
+-- printHelper s = " --" ++ (show $ (1 + length dots)) ++ "-> "  ++ [(head next)] ++ (printHelper $ drop 1 next) 
+--   where dots = takeWhile (\c -> c == '.') s
+--         next = dropWhile (\c -> c == '.') s
+
+getPaths :: Set.Set Pos -> KnD -> Char -> Char -> [(Integer, Integer)]
 getPaths op keys f t = aStar op from to
-  where from = snd $ fromJust $ find (\(c,_) -> c == f) keys
-        to = snd $ fromJust $ find (\(c,_) -> c == t) keys
+  where from = fromJust $ Map.lookup f keys
+        to   = fromJust $ Map.lookup t keys
 
-getAdjacentStr :: RawCave ->  Set.Set Pos -> [PoI] -> Char -> [String]
-getAdjacentStr rc op keys start = k
-  where k = map (printString rc) $ map (getPaths op keys start) symbols
+-- getAdjacentStr :: String -> RawCave ->  Set.Set Pos -> KnD -> Char -> [String]
+-- getAdjacentStr symb rc op keys start = k
+--   where k = map (printString rc) $ map (getPaths op keys start) symb
 
-type Graph = Map.Map Char (Map.Map Char Int)
-getAdjacent :: RawCave -> Set.Set Pos -> [PoI] -> Char -> Graph
-getAdjacent rc op keys start = Map.singleton start edgeMap
-  where ss = map (printRaw rc) $ map (getPaths op keys start) symbols           -- [String]
-        edge = concat $ map (maybeToList . parseToEdge) ss                      -- [(Char, String)]
-        edgeMap = Map.fromList edge
+-- type Graph = Map.Map Char (Map.Map Char Int)
+-- getAdjacent :: String ->  RawCave -> Set.Set Pos -> KnD -> Char -> Graph
+-- getAdjacent symb rc open places start = Map.singleton start edgeMap
+--   where edges = tedious symb rc open places start
+--         edgeMap = Map.fromList $ filter (\(_,d) -> d > 0) edges      -- Map Char Int
 
-buildAdjMap :: RawCave -> Set.Set Pos -> [PoI] -> Graph
-buildAdjMap rc op keys = Map.unions $ map (getAdjacent rc op keys) symbols
+-- buildAdjGraph :: String -> RawCave ->  Set.Set Pos -> KnD -> Graph
+-- buildAdjGraph symb rc open keys = Map.unions $ map (getAdjacent symb rc open keys) symb
 
--- tour :: ([Char], Char, Int) -> Int
--- tour (visited, current, dist)
+-- tedious :: [Char] -> RawCave -> Set.Set Pos -> KnD -> Char -> [(Char, Int)]
+-- tedious symb rc op places from = map (undoString from) t'
+--   where t = map (getPaths op places from) symb
+--         t' = map (map (\x -> fromJust $ Map.lookup x rc)) t
+
+type Graph2 = Map.Map Char (Map.Map Char (Int, String))
+getAdjacent2 :: String ->  RawCave -> Set.Set Pos -> KnD -> Char -> Graph2
+getAdjacent2 symb rc open places start = Map.singleton start edgeMap
+  where edges = tedious2 symb rc open places start
+        edgeMap = Map.fromList $ filter (\(_,(d,_)) -> d > 0) edges      -- Map Char Int
+
+buildAdjGraph2 :: String -> RawCave ->  Set.Set Pos -> KnD -> Graph2
+buildAdjGraph2 symb rc open keys = Map.unions $ map (getAdjacent2 symb rc open keys) (['a'..'z'] ++ ['@'])
+
+tedious2 :: [Char] -> RawCave -> Set.Set Pos -> KnD -> Char -> [(Char, (Int, String))]
+tedious2 symb rc op places from = map (undoString2 from) t'
+  where t = map (getPaths op places from) (['a'..'z'] ++ ['@'])                 -- [[(Integer,Integer)]]
+        t' = map (map (\x -> fromJust $ Map.lookup x rc)) t
+
+valid :: Map.Map Char (Int, String) -> String -> Map.Map Char (Int, String)
+valid m visited = Map.filter (\(_,s) -> length (s \\ vUpper) == 0) m
+  where vUpper = map (toUpper) visited
 --------------------------------------------------------------------------------
 main = do
   let s =  ["#################",
@@ -150,45 +183,64 @@ main = do
             "########.########",
             "#l.F..d...h..C.m#",
             "#################"]
-  -- f <- readFile "input_18.txt"
-  -- let s = lines f
-  let maxChar = 'z'
+  f <- readFile "input_18.txt"
+  let s = lines f
 
   let pp = parseInput 0 s
-  let rc = Map.filter (/= '#') pp
-  -- let op = Set.fromList $ Map.keys rc
-  let op = Set.fromList $ Map.keys $ Map.filter (`elem` ("@."++['a'..'z'])) rc
-  -- putStrLn $ show op
-  let keys = map (swap) $ Map.toList $ Map.filter (`elem` symbols) pp
+  let (open, places) = parseCave pp
 
+  let indices = (['a'..'z']++['A'..'Z']++"@") 
+  let open' = Set.union open (Set.fromList $ Map.elems places)
 
-  -- let (start, open, keys, doors) = parseCave pp
-  -- putStrLn $ show $ start
-  -- putStrLn $ show $ keys
-  -- putStrLn $ show $ foldr (Map.delete) keys "acegikmoqsuwxz"
-  -- putStrLn $ show $ doors
-  -- putStrLn $ show $ open
+  -- let graph = buildAdjGraph indices pp open' places
+  -- putStrLn $ show graph
 
-  -- let edges = buildAdjMap pp op keys
-  -- putStrLn $ show edges
-  -- putStrLn ""
-
-  -- let s = getAdjacentStr pp op keys '@'
-  -- mapM_ print s
-
-  let ((visited, current, dist),open) = (("", '@',0),op)
-  let remaining = ['a'..'p'] \\ (visited++[current])
-  let nDist = map (distKeys open keys current) remaining
-  putStrLn $ show nDist
-  let prs = filter (\(_,d) -> d > 0) $ zip remaining nDist 
-  putStrLn $ show visited
-  putStrLn $ show remaining
-  putStrLn $ show nDist
-  putStrLn $ show prs
-
-  let v' = map (\s -> visited ++ [s]) $ map (fst) prs
-  let d' = map (\d -> dist + d) $ map (snd) prs
-
-  putStrLn $ show $ zip3 v' (map (fst) prs) d'
+  let graph2 = buildAdjGraph2 indices pp open' places
+  putStrLn $ show graph2 
+ 
+  let s = tour2 50 graph2 (Set.empty, "@", 0)
+  putStrLn $ show s
 
   putStrLn $ show "END"
+
+-- type Graph = Map.Map Char (Map.Map Char Int)
+type TourState = (Set.Set Char, Char)
+
+-- tour :: Int -> Graph -> (Set.Set TourState, TourState, Int)-> Int
+-- tour timer graph (cover, (v, c), dist)
+--   | timer == 0              = dist
+--   | Set.size v == 16        = dist
+--   | Set.member (v, c) cover = 1000000
+--   | Map.size nbrsMap == 0   = 1000000
+--   | otherwise               = minimum $ map (tour (timer - 1) graph) next
+--   where !debugV = traceShowId $ (v, c, dist)
+--         nbrsMap = fromJust $ Map.lookup c graph -- Map Char Int
+--         nbrs = nbrsMap
+--         nbrsChars = Map.keys nbrs                     -- [Char]
+--         nbrsDists = Map.elems nbrs
+--         v' = map (\s -> if' (s `elem` ['a'..'z']) (Set.insert s v) v) nbrsChars
+--         -- v' = map (\s -> (Set.insert s v)) nbrsChars
+--         ts' = zip v' nbrsChars
+--         d' = map (\d -> dist + d) $ nbrsDists
+--         c' = repeat $ Set.insert (v, c) cover
+--         next = zip3 c' ts' d'
+
+tour2 :: Int -> Graph2 -> (Set.Set String, String, Int)-> Int
+tour2 timer graph (cover, visited, dist)
+  | timer == 0              = dist
+  | length visited == 27    = dist
+  -- | Set.member (v, c) cover = 1000000
+  | Map.size nrbsGood == 0   = 1000000
+  | otherwise               = minimum $ map (tour2 (timer - 1) graph) next
+  where !debugV = traceShowId $ (visited,dist)
+        cur = last visited -- most recent node visited Char
+        nbrsMap = fromJust $ Map.lookup cur graph -- get all nbrs of cur
+        nbrsNew = foldr (\vt m -> Map.delete vt m) nbrsMap visited -- remove previously visited vertices
+        nrbsGood = valid nbrsNew visited -- remove blocked vertices
+        nbrsChars = Map.keys nrbsGood  -- the next vertices to visit [Char]
+        nbrsDists = map (fst) $ Map.elems nrbsGood  -- the distance to the next vertex [Int]
+        v' = map (\c -> visited ++ [c]) nbrsChars
+        d' = map (\d -> dist + d) $ nbrsDists
+        c' = repeat $ Set.insert visited cover
+        next = zip3 c' v' d'
+
